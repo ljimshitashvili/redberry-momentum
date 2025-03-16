ac534d9eb28106aa302d8b8d2172723898b201d3
ljimshitashvili <jimshitashvili4444@gmail.com>
2025-03-16 21:36:56 +0400

    install react-router-dom

--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -1,9 +1,15 @@
-import { TaskPage } from "../pages";
+//Routes of the website is changed in this component. It contains Routes.
+
+import { AddNewEmployee, TaskPage } from "../pages";
+import { Route, Routes } from "react-router-dom";
 
 const Body = () => {
   return (
     <div className="w-full h-full ">
-      <TaskPage />
+      <Routes>
+        <Route path="/" element={<TaskPage />} />
+        <Route path="/add-employee" element={<AddNewEmployee />} />
+      </Routes>
     </div>
   );
 };
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,16 +1,18 @@
 import { add, logo } from "../assets";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="flex w-full justify-between">
       <img src={logo} alt="Logo" />
       <div className="button-container flex gap-[40px] ">
-        <button className="cursor-pointer border-[1px]">
+        <Link to="add-employee" className="cursor-pointer border-[1px]">
           თანამშრომლის შექმნა
-        </button>
-        <button className="flex cursor-pointer border-[1px]">
+        </Link>
+        <Link className="flex cursor-pointer border-[1px]">
           <img src={add} alt="Plus Icon" />
           შექმენი ახალი დავალება
-        </button>
+        </Link>
       </div>
     </div>
   );
